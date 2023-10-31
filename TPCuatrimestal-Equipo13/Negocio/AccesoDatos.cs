3e15c548545650c40200ff9a544ead53f9b12365
matinicotra <matiasanicotra@gmail.com>
2023-10-31 09:54:25 -0300

    creacion Negocio y clase de acceso a BD

---
+++ TPCuatrimestal-Equipo13/Negocio/AccesoDatos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    internal class AccesoDatos
+    {   
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection();
+            comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            conexion.ConnectionString = "server= .\\SQLEXPRESS; database=XXXXXXXXXXXXX; integrated security=true";  // FALTA NOMBRE BD 
+        }
+
+        public SqlDataReader Lector { get { return lector; } }
+
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandText = consulta;
+        }
+
+        public void SetearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void EjecutarConsulta()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void EjecutarAccion()
+        {
+            try
+            {
+                comando.Connection = conexion;
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if (!(lector == null)) lector.Close();
+
+            conexion.Close();
+        }
+    }
+}

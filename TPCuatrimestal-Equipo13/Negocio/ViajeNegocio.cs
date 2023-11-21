e576b96a4e9be4763a4cc29b8b1a7d931fbe162c
matinicotra <matiasanicotra@gmail.com>
2023-11-21 16:39:20 -0300

    alta viaje

--- TPCuatrimestal-Equipo13/Negocio/ViajeNegocio.cs
+++ TPCuatrimestal-Equipo13/Negocio/ViajeNegocio.cs
@@ -94,6 +94,37 @@ namespace Negocio
             }
         }
 
+        public void AltaViaje(Viaje viaje)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("INSERT INTO VIAJES (IDCHOFER, IDCLIENTE, TIPOVIAJE, IMPORTE, IDDOMORIGEN, IDDOMDESTINO1, IDDOMDESTINO2, IDDOMDESTINO3, ESTADO, FECHAHORAVIAJE, PAGADO, MEDIODEPAGO)");
+                datos.SetearParametro("@IDCHOFER", viaje.IDChofer);
+                datos.SetearParametro("@IDCLIENTE", viaje.IDCliente);
+                datos.SetearParametro("@TIPOVIAJE", viaje.TipoViaje);
+                datos.SetearParametro("@IDDOMORIGEN", viaje.Origen.IDDomicilio);
+                datos.SetearParametro("@IDDOMDESTINO1", viaje.Destinos[0].IDDomicilio);
+                datos.SetearParametro("@IDDOMDESTINO2", viaje.Destinos[1].IDDomicilio);
+                datos.SetearParametro("@IDDOMDESTINO3", viaje.Destinos[2].IDDomicilio);
+                datos.SetearParametro("@ESTADO", viaje.Estado);
+                datos.SetearParametro("@FECHAHORAVIAJE", "GETDATE()");       /// funciona????
+                datos.SetearParametro("@PAGADO", viaje.Pagado);
+                datos.SetearParametro("@MEDIODEPAGO", viaje.MedioDePago);
+
+                datos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
+
 
     }
 }
